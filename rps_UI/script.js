65fb3fd85c0efaae98e1ed1364365d0e772d6e36
theXsingularity <jdspaziano@gmail.com>
2022-03-27 12:06:21 -0400

    one round working with text on page

--- rps_UI/script.js
+++ rps_UI/script.js
@@ -1,11 +1,23 @@
+let computerMove;
+let playerMove;
+
 function computerPlay() {
     let plays = ["rock", "paper", "scissors"];
-    return plays[Math.floor(Math.random() * plays.length)];
+    let play = plays[Math.floor(Math.random() * plays.length)];
+    cM.textContent = `Computer has selected: ${play}`;
+    return play;
+    
 }
 
-let playerMove = "";
-let computerMove = "";
+function getPlay(button){
+    playerMove=button.getAttribute("data-move");
+    console.log(`you chose ${playerMove}`);
+    pM.textContent = `You have Selected: ${playerMove}`;
+    cM.textContent = "Push play for computer to make selection";
+  }
 
+let computer = 0;
+let player = 0;
 
 function playGame(playerMove, computerMove) {
     if ((playerMove == "rock" && computerMove == "scissors")
@@ -13,33 +25,37 @@ function playGame(playerMove, computerMove) {
     || (playerMove == "scissors" && computerMove == "paper"))
     { 
         player++; //add to player score
-        return `You win! ${playerMove} beats ${computerMove}`;
+        result.textContent =  `Results: You win! ${playerMove} beats ${computerMove}`;
     } else if ((playerMove == "rock" && computerMove == "paper")
     || (playerMove == "paper" && computerMove == "scissors")
     || (playerMove == "scissors" && computerMove == "rock"))
     {
         computer++; //add to computer score
-        return `You Lose! ${computerMove} beats ${playerMove}`;
+        result.textContent =  `Results: You Lose! ${computerMove} beats ${playerMove}`;
     } else {
-        return "It's a tie!";
+        result.textContent = "Results: It's a tie!";
     }
-}
-
-function getPlay(button){
-  playerMove=button.getAttribute("data-move");
-  console.log(`you chose ${playerMove}`);
 }
 
 
 function playRound(button){
-  console.log(playerMove);
-  console.log(computerPlay());
+  console.log(playGame(playerMove, computerPlay()));
+  console.log(computer);
 }
 
 const container = document.querySelector('#container');
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = playerMove; // why doesnt this work...
+const pM = document.createElement('div');
+pM.classList.add('pM');
+pM.textContent = "Please Make a Selection...";
+container.appendChild(pM);
 
-container.appendChild(content);
+const cM = document.createElement('div');
+cM.classList.add('cM');
+cM.textContent = "Push play for computer to make selection";
+container.appendChild(cM);
+
+const result = document.createElement('div');
+result.classList.add('result');
+result.textContent = "Results: ";
+container.appendChild(result);
